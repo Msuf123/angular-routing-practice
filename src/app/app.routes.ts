9e9b13dfa7819b180f2c7dc2be61dfb0efefda7d
Msuf123 <akshatmalik18t@gmail.com>
2024-05-24 01:52:34 +0530

    Understood about the error componet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AboutComponent } from './about/about.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [{
     path:'',component:HomepageComponent
 },{
     path:'about/:id/:name',component:AboutComponent
-}
+},
+{path:'**',component:ErrorComponent}
 ];
