f69d7b29fe526977f3957901d92b890b1da734d0
Msuf123 <akshatmalik18t@gmail.com>
2024-06-15 23:00:08 +0530

    Confirm

--- src/app/guard/deactivate.guard.ts
+++ src/app/guard/deactivate.guard.ts
@@ -2,6 +2,7 @@ import { CanActivateFn } from '@angular/router';
 
 export const deactivateGuard: CanActivateFn = (route, state) => {
   let a=confirm('Exit wihotu saving')
+  //return true or false
   console.log(a)
   return a;
 };
